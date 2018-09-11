a0760e2c0f77ce4f07e05e04aedfbdef76267ac8
Vince Liem <vincepaul.liem@gmail.com>
2018-09-11 14:14:00 +0200

    Shopping cart updated to accompany departments

--- store.js
+++ store.js
@@ -1,15 +1,5 @@
 // TODO:
 
-// var products = [
-//   { referenceNumber: 1231, name: "Super Lite Mat", price: 10 },
-//   { referenceNumber: 1232, name: "Power Mat", price: 20 },
-//   { referenceNumber: 1233, name: "Block", price: 30 },
-//   { referenceNumber: 1234, name: "Meditation cushion", price: 30 },
-//   { referenceNumber: 1235, name: "The best T-shirt", price: 200 },
-//   { referenceNumber: 1236, name: "The cutest yoga pants", price: 300 },
-//   { referenceNumber: 1237, name: "Bring Yoga To Life", price: 30 },
-//   { referenceNumber: 1238, name: "Light On Yoga", price: 10 }
-// ]
 
 var departments = [
   {
